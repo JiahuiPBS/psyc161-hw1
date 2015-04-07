560a9d5ac99df13556d65df94c35129bc763d2de
Jiahui <Jiahui.Guo.GR@dartmouth.edu>
2015-04-07 00:42:09 -0400

    compare time, non-recursive

---
+++ comp_time_with_plot.py
@@ -0,0 +1,52 @@
+#!/user/bin/env python
+
+import time
+
+def factorial_recursive(n):
+        if n == 0:
+            return 1
+        else:
+            return n * factorial_recursive(n-1)
+def factorial_nonrecursive(n):
+        if n == 0:
+            result = 1
+        else:
+            result = 1
+            i = 1
+            while i < (n + 1):
+                result *= i
+                i += 1
+        return result
+        
+        
+def comp_time(m):
+    startf = time.clock()
+    i = 1
+    while i < 10000:
+        factorial_recursive(m)
+        i += 1
+    elapsedf = time.clock() - startf
+
+    startfn = time.clock()
+    j = 1
+    while j < 10000:
+        factorial_nonrecursive(m)
+        j += 1
+    elapsedfn = time.clock() - startfn
+
+    ratio = elapsedf/elapsedfn
+    return ratio
+
+if __name__ == '__main__':
+    # This is a way to determine either file was "executed", so if it was
+    # imported (by e.g. nose) as a library, we should not run code
+    # below
+
+    import matplotlib.pyplot as pl
+    tratios = []
+    ns = range(10, 85, 5)
+    for n in ns:
+        tratio = comp_time(n)
+        tratios.append(tratio)
+    pl.plot(ns, tratios)
+    pl.show()
---
+++ factorial_non.py
@@ -0,0 +1,29 @@
+#!/user/bin/env python
+
+from nose.tools import assert_equal
+
+def factorial_nonrecursive(n):
+    if n == 0:
+        result = 1
+    else:
+        result = 1
+        i = 1
+        while i < (n + 1):
+            result *= i
+            i += 1
+    return result
+
+def test_factorial():
+    assert_equal(factorial_nonrecursive(1), 1)
+    assert_equal(factorial_nonrecursive(0), 1)
+    assert_equal(factorial_nonrecursive(5), 120)
+    # TODO: add more
+
+if __name__ == '__main__':
+    # This is a way to determine either file was "executed", so if it was
+    # imported (by e.g. nose) as a library, we should not run code
+    # below
+
+    nconditions = raw_input("Please enter number of conditions: ")
+    norders = factorial_nonrecursive(int(nconditions))
+    print("Number of possible trial orders: " + str(norders))
